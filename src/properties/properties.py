a17ee17f72bd546831b7421c191f7b94823f03a0
Sebastian Hegardt <se.hegardt@gmail.com>
2020-01-29 12:11:03 +0900

    Added user properties to FloatFactor sockets

--- src/properties/properties.py
+++ src/properties/properties.py
@@ -2,61 +2,73 @@ import bpy
 from bpy.props import IntProperty, BoolProperty, FloatProperty
 from bpy.types import PropertyGroup
 
+
 class PG_PublicProps(PropertyGroup):
-    x_res : IntProperty(
-        name="X Resolution",
-        description="X resolution of renders",
-        min=1,
-        default=128
+    x_res: IntProperty(
+        name="X Resolution", description="X resolution of renders", min=1, default=128
     )
 
     y_res: IntProperty(
-        name="Y Resolution",
-        description="Y resolution of renders",
-        min=1,
-        default=128
+        name="Y Resolution", description="Y resolution of renders", min=1, default=128
     )
 
     render_amount: IntProperty(
-        name="Amount",
-        description="Total amount of images to render",
-        min=1,
-        default=5
+        name="Amount", description="Total amount of images to render", min=1, default=5
     )
+
 
 class FLOAT_SOCKET_PG_UserProperties(PropertyGroup):
     user_min: FloatProperty(
         name="User Min",
-        description="The minimum value that this input parameter will take during render.",
+        description="The minimum value that this input parameter will take during render",
+        default=1,
     )
 
     user_max: FloatProperty(
         name="User Max",
-        description="The maximum value that this input parameter will take during render.",
+        description="The maximum value that this input parameter will take during render",
+        default=1,
     )
+
+
+class FLOAT_FACTOR_SOCKET_PG_UserProperties(PropertyGroup):
+    user_min: FloatProperty(
+        name="User Min",
+        description="The minimum value that this input parameter will take during render",
+        min=0,
+        max=1,
+    )
+
+    user_max: FloatProperty(
+        name="User Max",
+        description="The maximum value that this input parameter will take during render",
+        min=0,
+        max=1,
+    )
+
 
 class COLOR_SOCKET_PG_UserProperties(PropertyGroup):
     user_min: FloatProperty(
         name="User Min",
-        description="The minimum value that this input parameter will take during render.",
+        description="The minimum value that this input parameter will take during render",
     )
 
     user_max: FloatProperty(
         name="User Max",
-        description="The maximum value that this input parameter will take during render.",
+        description="The maximum value that this input parameter will take during render",
     )
 
 
 class PG_InternalProps(PropertyGroup):
     current_render: IntProperty(
         name="Current Render",
-        description="The id number of the image currently running.",
+        description="The id number of the image currently running",
         min=1,
         default=1,
     )
 
     running: BoolProperty(
         name="Running",
-        description="Indicates if the script is currently running.",
-        default=False
+        description="Indicates if the script is currently running",
+        default=False,
     )
